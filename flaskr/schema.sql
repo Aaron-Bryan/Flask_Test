f652e56e12cfe5e85e0ee8d91ab5d533d4c79a75
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-06-07 18:40:46 +0800

    Testing Stuff

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -2,3 +2,17 @@
 
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+    id INTEGER,
+    username TEXT,
+    password TEXT
+    );
+
+CREATE TABLE post (
+    id INTEGER,
+    author_id INTEGER,
+    created TIMESTAMP,
+    post_title TEXT,
+    post_body TEXT
+    );
