f817f5385ed5db8513d8a7a67e7d31a3cac944b1
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-06-21 19:39:21 +0800

    Testing Stuff

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -4,3 +4,5 @@ from flaskr.db import get_db
 
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
