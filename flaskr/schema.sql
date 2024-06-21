2df7da6ea48594b026d4e3b98692785659fc8bfe
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-06-21 19:59:38 +0800

    Making stuff more readable

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -14,11 +14,18 @@ CREATE TABLE user (
     password TEXT NOT NULL
     );
 
+--Creates a Table called post
 CREATE TABLE post (
+    --Primary key is id and is also automatically generated
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    --author_id must be integer and not null
     author_id INTEGER NOT NULL,
+    --created is a date/time value
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    --post_title is a text value that must not be null
     post_title TEXT NOT NULL,
+    --post_title is a text value that must not be null
     post_body TEXT NOT NULL,
+    --sets the author_id as a foreign key that references the id column from the user table
     FOREIGN KEY(author_id) REFERENCES user(id)
     );
