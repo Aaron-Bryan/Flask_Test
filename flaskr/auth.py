3122be2933ed9f2f28355bcbf91ca857c1b50a4a
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-06-22 17:39:30 +0800

    Added comments for readability

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,10 +10,12 @@ from werkzeug.security import check_password_hash, generate_password_hash
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 #Function for new user registration
+#Associates the URL "/register" with the register view function
 @bp.route("/register", methods=("GET", "POST"))
 def register():
     #"POST" the new user's data into the database
     if (request.method == "POST"):
+        #"request.form" is a special dict that maps the respective inputs
         username = request.form["username"]
         password = request.form["password"]
 
@@ -28,15 +30,24 @@ def register():
 
         #If params are satisfied
         if error is None:
+            #Insert the new user's data into the database
             try:
+                #"db.execute" takes a sql query with the "?" as a placeholder
+                #It's good practice to not directly input passwords into the database, hash them insteam with generate_password_hash
                 db.execute("Insert Into user (username, password) Values (?, ?)",
                            (username, generate_password_hash(password)))
+                #"db.commit" needs to be called to save the changes made
                 db.commit()
+            #An IntegrityError happens when the username already exists within the databse
             except db.IntegrityError:
                 error = f"User {username} already exists"
             else:
+                #After storing the new user's data into the database they are redirected to the login page
+                #"redirect" generates a redirect response to the generated URL
+                #"url_for" generates URL to the login view
                 return redirect(url_for("auth.login"))
 
+        #Ouputs the error message
         flash(error)
 
     return render_template("auth/register.html")
